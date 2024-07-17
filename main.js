d61247ca9874d91cde16a11416af269c7b1a5169
unknown <pacman16012004@gmail.com>
2024-07-17 11:24:15 +0700

    sua js

--- main.js
+++ main.js
@@ -1,35 +1,83 @@
-var currentPage = 1;
+var currentPage;
 var dots = document.getElementsByClassName("dot");
 var feedbacks = document.getElementsByClassName("feedback-item");
 dots[1].onclick = function () {
     [...feedbacks].forEach(item => {
         item.className = "feedback-item from-1-to-2";
     });
-    currentPage = 2;
+    for (let i = 0; i <= 8; i++) {
+        if (i == 4) {
+            dots[i].className = "dot active";
+        }
+        else {
+            dots[i].className = "dot"
+        }
+    }
+    console.log(currentPage);
 }
 dots[2].onclick = function () {
     [...feedbacks].forEach(item => {
         item.className = "feedback-item from-1-to-3";
     });
-    currentPage = 3;
+    currentPage = 2;
+    for (let i = 0; i <= 8; i++) {
+        if (i == 8) {
+            dots[i].className = "dot active";
+        }
+        else {
+            dots[i].className = "dot"
+        }
+    }
 }
 dots[3].onclick = function () {
     [...feedbacks].forEach(item => {
         item.className = "feedback-item";
     });
+    for (let i = 0; i <= 8; i++) {
+        if (i == 0) {
+            dots[i].className = "dot active";
+        }
+        else {
+            dots[i].className = "dot"
+        }
+    }
 }
 dots[5].onclick = function () {
     [...feedbacks].forEach(item => {
         item.className = "feedback-item from-1-to-3";
     });
+    for (let i = 0; i <= 8; i++) {
+        if (i == 8) {
+            dots[i].className = "dot active";
+        }
+        else {
+            dots[i].className = "dot"
+        }
+    }
 }
 dots[6].onclick = function () {
     [...feedbacks].forEach(item => {
         item.className = "feedback-item";
     });
+    for (let i = 0; i <= 8; i++) {
+        if (i == 0) {
+            dots[i].className = "dot active";
+        }
+        else {
+            dots[i].className = "dot"
+        }
+    }
 }
 dots[7].onclick = function () {
     [...feedbacks].forEach(item => {
         item.className = "feedback-item from-1-to-2";
     });
+    for (let i = 0; i <= 8; i++) {
+        if (i == 4) {
+            dots[i].className = "dot active";
+        }
+        else {
+            dots[i].className = "dot"
+        }
+    }
 }
